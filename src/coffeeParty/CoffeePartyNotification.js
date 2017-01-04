ad97d15ab2dc3f48981a669765e830b9aa3aa862
borrys <andrzej.kimel@gmail.com>
2017-01-04 18:14:12 +0100

    coffee party starter and notification styles

--- src/coffeeParty/CoffeePartyNotification.js
+++ src/coffeeParty/CoffeePartyNotification.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import moment from 'moment';
+import style from './style.scss';
 
 class CoffeePartyContainer extends React.Component {
   render() {
     const timer = moment(this.props.time).format('mm:ss');
 
     if (this.props.visible) {
-      return <div>
+      return <div className='coffee-notification'>
         <h1>Coffee party started</h1>
-        {timer}
+        <div className='timer'>{timer}</div>
       </div>;
     }
     return <div />;
