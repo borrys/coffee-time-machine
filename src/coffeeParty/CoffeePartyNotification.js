02a678f1005278a6ac5deca4526369da2b82cbe4
borrys <andrzej.kimel@gmail.com>
2017-01-05 20:24:08 +0100

    handling background click in Modal

--- src/coffeeParty/CoffeePartyNotification.js
+++ src/coffeeParty/CoffeePartyNotification.js
@@ -8,7 +8,7 @@ class CoffeePartyContainer extends React.Component {
     const timer = moment(this.props.time).format('mm:ss');
 
     if (this.props.visible) {
-      return <Modal>
+      return <Modal onClick={this.props.dismiss}>
         <div className='coffee-notification'>
           <h1>Coffee party started</h1>
           <div className='timer'>{timer}</div>
--- src/commons/modal/Modal.js
+++ src/commons/modal/Modal.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import style from './style.scss';
 
+
 export default class Modal extends React.Component {
   render() {
-    return <div className='modal'>
+    return <div className='modal' onClick={this.onClick.bind(this)}>
       {this.props.children}
     </div>;
+  }
+
+  onClick(event) {
+    event.stopPropagation();
+    if (event.target === event.currentTarget) {
+      this.props.onClick();
+    }
   }
 }
