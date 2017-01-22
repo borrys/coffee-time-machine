8f872c39334c90b019fa8fb9784066912182824f
borrys <andrzej.kimel@gmail.com>
2017-01-22 19:02:54 +0100

    button for github authorization

--- src/auth/Github.js
+++ src/auth/Github.js
@@ -5,6 +5,11 @@ import './style.scss';
 export default class Github extends React.Component {
   render() {
     return <div className='github'>
+      <button className='authorize'  onClick={this.githubAuth.bind(this)}>Sign In</button>
     </div>;
+  }
+
+  githubAuth() {
+    window.open('http://localhost:5050/auth', '_self');
   }
 }
